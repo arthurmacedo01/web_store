0489e4f98452bfc3c19fc4714df19bafb23ad96b
Arthur Macedo <arthurmacedo01@gmail.com>
2022-12-16 15:42:54 -0300

    add: pix image

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,10 @@ class OrdersController < StoreController
 
   def show
     @order = Spree::Order.find_by!(number: params[:id])
-    authorize! :show, @order, cookies.signed[:guest_token]
+    authorize! :show, @order, cookies.signed[:guest_token]    
+    if( get_payment_type == "SolidusPay::Transaction")
+      get_qr_codes
+    end
   end
 
   private
@@ -20,5 +23,16 @@ class OrdersController < StoreController
 
   def store_guest_token
     cookies.permanent.signed[:guest_token] = params[:token] if params[:token]
+  end
+
+  def get_payment_type
+    Spree::Payment.find_by(order_id: @order[:id], state: "pending")[:source_type]
+  end
+
+  def get_qr_codes 
+    transaction_id = Spree::Payment.where(order_id: 30, state: "pending").first[:source_id]
+    transaction = SolidusPay::Transaction.find(transaction_id)
+    @qr_code = transaction[:qr_code]
+    @qr_code_base64 = transaction[:qr_code_base64]
   end
 end
